123911619250b718eb6555b90a99fd1f808b93c4
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-12-28 16:31:31 +0100

    new

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 
-namespace LUSC_e_Maintenance
+namespace LUSCMaintenance
 {
     public class Program
     {
