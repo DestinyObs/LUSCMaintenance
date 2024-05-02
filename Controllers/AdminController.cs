12cbf29e15db41dce5aadbc690ca303958d2ffa3
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2024-05-02 23:42:03 +0100

    new

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LUSCMaintenance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+    }
+}
